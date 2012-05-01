bf589664af637d8c4f1b6a8e1f4d4d297603fc9e
Kyle Conroy <kyle.j.conroy@gmail.com>
2012-05-01 11:58:54 -0700

    PEP8 clean up. Add more meaningful names

--- congressbot.py
+++ congressbot.py
@@ -1,31 +1,47 @@
 #!/usr/bin/python
-
+import logging
 import reddit
 import feedparser
 import time
 from pymongo import Connection
 
-mdb = Connection()
-wcdb = mdb.wc
-hclc = wcdb.wc_house_today
+mongo_db = Connection()
+congress_db = mongo_db.wc
+house_collection = congress_db.wc_house_today
 
 r = reddit.Reddit(user_agent='WatchingCongress/1.0')
 r.login('congressbot', '<BOTPASS>')
 
-hf = feedparser.parse('http://www.govtrack.us/events/events.rss?feeds=misc%3Aintroducedbills')
+govfeed = feedparser.parse('http://www.govtrack.us/events/events.rss?'
+                           'feeds=misc%3Aintroducedbills')
 
+POST_TEMPLATE = """
+{description}
 
-for entry in hf.entries:
-  if (entry['guid'].find('guid')):
-    if hclc.find_one({'guid': entry['guid']}) == None:
-      record = {'title': entry['title'],
+[Govtrack.us Summary]({link})
+"""
+
+for entry in govfeed.entries:
+
+    if not entry['guid'].find('guid'):
+        continue
+
+    if house_collection.find_one({'guid': entry['guid']}):
+        continue
+
+    record = {
+        'title': entry['title'],
         'description': entry['description'],
         'link': entry['link'],
-        'guid': entry['guid']}
-      try:
-        r.submit('watchingcongress', entry['title'], text=entry['description'] +
-          "\n\n[Govtrack.us Summary](" + entry['link'] + ")")
-        hclc.insert(record)
-      except:
-        print "exception occured"
-      time.sleep(2)
+        'guid': entry['guid'],
+    }
+
+    try:
+        text = SELF_POST.format(description=entry['description'],
+                                link=entry['link'])
+        r.submit('watchingcongress', entry['title'], text=text)
+        house_collection.insert(record)
+        logging.info("Created story: {}".format(entry['title']))
+    except Exception as e:
+        logging.error("Exception occured: {}".format(unicode(e)))
+        time.sleep(2)
